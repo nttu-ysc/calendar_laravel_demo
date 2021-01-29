3004c8df1a351fc14b41592831d5b4e3ce74bb31
Danny Cheng <a7932677@gmail.com>
2021-01-30 07:21:44 +0800

    + events table

---
+++ database/migrations/2021_01_29_214931_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title', 100);
+            $table->integer('year');
+            $table->integer('month');
+            $table->integer('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('events');
+    }
+}
