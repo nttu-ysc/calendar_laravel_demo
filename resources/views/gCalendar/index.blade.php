32f02fea1cb2a4a0896b80ae9813ecf6db622ff5
Danny Cheng <a7932677@gmail.com>
2021-02-03 03:06:52 +0800

    + google calendar view

---
+++ resources/views/gCalendar/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.frontend')
+
+@section('calendar')
+<div id="calendar" data-year="{{$year}}" data-month="{{$month}}">
+    <div id="header">
+        {{$year}}/{{$month}}
+    </div>
+    <div id="days" class="clearfix">
+        <div class="day">SUN</div>
+        <div class="day">MON</div>
+        <div class="day">TUE</div>
+        <div class="day">WED</div>
+        <div class="day">THR</div>
+        <div class="day">FRI</div>
+        <div class="day">SAT</div>
+    </div>
+
+    <div id="date" class="clearfix" data-id="" data-from="">
+        @foreach ($dates as $key => $date)
+        <div class="date-block @if(!$date)empty @endif" @if($date) data-date="{{$date}}" @endif>
+            <div class="date">
+                {{$date}}
+            </div>
+            <div class="events">
+                @foreach ($events as $event)
+                @if (($event->start->date[2] == $date) && ($event->start->date[1] == $month) &&
+                ($event->start->date[0] == $year))
+                <div class="event clearfix" data-id="{{$event->id}}" data-from="{{$event->start->dateTime}}">
+                    <div class="title">{{$event->summary}}</div>
+                    <div class="from">{{$event->start->dateTime}}</div>
+                </div>
+                @endif
+                @endforeach
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
+
+@section('info-panel')
+<div id="info-panel" class="clearfix">
+    <div class="close">X</div>
+    <form>
+        <input type="hidden" name="id">
+        <div class="title">
+            <label>event</label><br>
+            <input type="text" name="title">
+        </div>
+        <div class="error-msg">
+            <div class="alert alert-danger"></div>
+        </div>
+        <div class="time-picker">
+            <div class="selected-date">
+                <span class="month"></span>/<span class="date"></span>
+                <input type="hidden" name="year">
+                <input type="hidden" name="month">
+                <input type="hidden" name="date">
+            </div>
+            <div class="form">
+                <label for="from">from</label><br>
+                <input id="from" type="time" name="start_time">
+            </div>
+            <div class="to">
+                <label for="to">to</label><br>
+                <input id="to" type="time" name="end_time">
+            </div>
+        </div>
+        <div class="description">
+            <label>description</label><br>
+            <textarea name="description" id="description"></textarea>
+        </div>
+    </form>
+    <div class="buttons clearfix">
+        <button class="create">create</button>
+        <button class="update">update</button>
+        <button class="cancel">cancel</button>
+        <button class="delete">delete</button>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
+        @if (request()->is('Gcalendar'))
+        <a class="navbar-brand" href="/Gcalendar">Google Calendar</a>
+        @else
         <a class="navbar-brand" href="index.php">Calendar</a>
+        @endif
         <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav ">
+                @if (request()->is('Gcalendar'))
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Go To Calendar</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="/Gcalendar">Go To Google Calendar</a>
+                </li>
+                @endif
+                @if (Auth::check())
                 <li class="nav-item">
                     <a class="nav-link">{{Auth::user()->name}}</a>
                 </li>
@@ -13,6 +27,7 @@
                                                 this.closest('form').submit();">Log Out</a>
                     </form>
                 </li>
+                @endif
             </ul>
         </div>
     </div>
